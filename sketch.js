ee1abbb75fdff7f76cd6e9b826246932c11bff2b
Martina <martinamelillo97@gmail.com>
2019-09-30 14:21:52 +0200

    sketch

--- sketch.js
+++ sketch.js
@@ -1,11 +1,47 @@
+var x = 0;
+var y = 0;
+var space = 80;
+
 function preload(){
-  // put preload code here
 }
 
 function setup() {
-  // put setup code here
+
+createCanvas(windowWidth, windowHeight);
+
+background(0);
+
+frameRate(6);
+
 }
 
 function draw() {
-  // put drawing code here
+
+stroke(255);
+
+for(var x = 0; x < windowWidth; x += space){
+if(random() < 0.5){
+line(x, y, x + space, y + space);
+} else{
+line(x, y + space, x + space, y);
 }
+}
+x = x + space;
+if(x > windowWidth){
+  x = 0;
+  y = y + space;
+}
+
+if(y > windowHeight + 500){
+  textAlign(CENTER,CENTER);
+  textSize(20);
+  fill(255);
+  text('PRESS FOR A NEW TEXTURE', windowWidth/2,windowHeight/2);
+}
+}
+
+function mousePressed() {
+  background(0);
+  y = 0;
+  redraw();
+ }
